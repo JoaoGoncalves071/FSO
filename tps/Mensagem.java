59fe01912022d2615bf6d7d20388261b05d3effb
João Gonçalves <joaomariamalhogoncalves@gmail.com>
2022-10-04 16:17:55 +0100

    v1.0 Beta

--- tps/Mensagem.java
+++ tps/Mensagem.java
@@ -2,4 +2,27 @@ package tps;
 
 public abstract class Mensagem {
     public int num;
+    public int type;
+    public String text;
+
+    public int getNum(){
+        return this.num;
+    }
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public int getType() {
+        return type;
+    }
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
 }
