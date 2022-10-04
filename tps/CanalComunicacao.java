82b17c8f9fd44d9151c56c2a134bfff7bfa6c1e3
João Gonçalves <joaomariamalhogoncalves@gmail.com>
2022-10-04 14:21:25 +0100

    First commit

--- tps/CanalComunicacao.java
+++ tps/CanalComunicacao.java
@@ -9,96 +9,95 @@ import java.nio.channels.FileChannel;
 import java.util.Iterator;
 
 public class CanalComunicacao {
-	//ficheiro
-	private File ficheiro;
+    //ficheiro
+    private File ficheiro;
 
-	// canal que liga o conteúdo do ficheiro ao buffer
-	private FileChannel canal;
+    // canal que liga o conteúdo do ficheiro ao buffer
+    private FileChannel canal;
 
-	//buffer
-	private MappedByteBuffer buffer;
+    //buffer
+    private MappedByteBuffer buffer;
 
-	// dimensão máxima em bytes do buffer
-	final int BUFFER_MAX = 30;
+    // dimensão máxima em bytes do buffer
+    final int BUFFER_MAX = 30;
 
-	// construtor onde se cria o canal
-	public CanalComunicacao() {
+    // construtor onde se cria o canal
+    public CanalComunicacao() {
 
-		//Construtor
-		ficheiro = null;
-		canal = null;
-		buffer = null;
-		
-	}
-	
-	
-	/*
-	 * Método que abre canal de comunicacao
-	 */
-	
-	public boolean abrirCanal() {
-		//cria um ficheiro com o nome comunicacao.dat
-		ficheiro = new File("comunicacao.dat");
-		
-		//cria um canal de comunicação de leitura e escrita, "rw"
-		try {
-			canal = new RandomAccessFile(ficheiro, "rw").getChannel();
-		}catch (FileNotFoundException e) {
-			return false;
-		}
-		
-		//mapeia para memória o conteúdo do ficheiro
-		try {
-			buffer = canal.map(FileChannel.MapMode.READ_WRITE, 0, BUFFER_MAX);
-		}catch (IOException e) {
-			return false;
-		}
-				
-		return true;
-		
-	}
-	
-	
-	/*
-	 * recebe uma mensagem convertendo-a numa String
-	 */
-	public String receberMensagem() {
-		String msg = new String();
-		buffer.position(0);
-		char c = buffer.getChar();
-		while(c != '\0') {
-			msg +=c;
-		}
-		return msg; 
-	}
-	
-	
-	/*
-	 * envia uma String como mensagem
-	 */
-	public void enviarMensagem(String msg) {
-		char c;
-		buffer.position(0);
-		for(int i=0; i<msg.length(); ++i) {
-			c = msg.charAt(i);
-			buffer.putChar(c);
-		}
-		buffer.putChar('\0');
-	}
-	
-	/*
-	 * fecha o canal de comunicação
-	 */
-	public void fecharCanal() {
-		if(canal!=null) {
-			try {
-				canal.close();
-			} catch (Exception e) {
-				canal = null;
-			}
-		}
-	}
-	
-	
+        //Construtor
+        ficheiro = null;
+        canal = null;
+        buffer = null;
+
+    }
+
+
+    /*
+     * Método que abre canal de comunicacao
+     */
+
+    public boolean abrirCanal() {
+        //cria um ficheiro com o nome comunicacao.dat
+        ficheiro = new File("comunicacao.dat");
+
+        //cria um canal de comunicação de leitura e escrita, "rw"
+        try {
+            canal = new RandomAccessFile(ficheiro, "rw").getChannel();
+        } catch (FileNotFoundException e) {
+            return false;
+        }
+
+        //mapeia para memória o conteúdo do ficheiro
+        try {
+            buffer = canal.map(FileChannel.MapMode.READ_WRITE, 0, BUFFER_MAX);
+        } catch (IOException e) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+
+    /*
+     * recebe uma mensagem convertendo-a numa String
+     */
+    public String receberMensagem() {
+        String msg = new String();
+        buffer.position(0);
+        char c = buffer.getChar();
+        while (c != '\0') {
+            msg += c;
+        }
+        return msg;
+    }
+
+
+    /*
+     * envia uma String como mensagem
+     */
+    public void enviarMensagem(String msg) {
+        char c;
+        buffer.position(0);
+        for (int i = 0; i < msg.length(); ++i) {
+            c = msg.charAt(i);
+            buffer.putChar(c);
+        }
+        buffer.putChar('\0');
+    }
+
+    /*
+     * fecha o canal de comunicação
+     */
+    public void fecharCanal() {
+        if (canal != null) {
+            try {
+                canal.close();
+            } catch (Exception e) {
+                canal = null;
+            }
+        }
+    }
+
 
 }
--- tps/CanalComunicacaoComLocks.java
+++ tps/CanalComunicacaoComLocks.java
@@ -1,9 +1,23 @@
 package tps;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 
 public class CanalComunicacaoComLocks extends CanalComunicacao {
+
+    private File ficheiro;
+
+    // canal que liga o conteúdo do ficheiro ao buffer
+    private FileChannel canal;
+
+    //buffer
+    private MappedByteBuffer buffer;
+
+    // dimensão máxima em bytes do buffer
+    final int BUFFER_MAX = 30;
 
     @Override
     public String receberMensagem() {
@@ -12,40 +26,39 @@ public class CanalComunicacaoComLocks extends CanalComunicacao {
         try {
             l = this.canal.lock();
             return super.receberMensagem();
-        }
-        catch (IOException iEx) {
+        } catch (IOException iEx) {
             throw new IllegalArgumentException("Erro ao obter o lock");
-        }
-        finally {
-            if (l != null){
+        } finally {
+            if (l != null) {
                 try {
                     l.release();
-                } catch (IOException iEx){
+                } catch (IOException iEx) {
                     throw new IllegalArgumentException("Erro ao liberar o lock");
                 }
             }
         }
     }
 
+    @Override
     public void enviarMensagem(String msg) {
         FileLock l = null;
 
         try {
             l = this.canal.lock();
             super.enviarMensagem(msg);
-        }
-        catch (IOException iEx) {
+        } catch (IOException iEx) {
             throw new IllegalArgumentException("Erro ao obter o lock");
-        }
-        finally {
-            if (l != null){
+        } finally {
+            if (l != null) {
                 try {
                     l.release();
-                } catch (IOException iEx){
+                } catch (IOException iEx) {
                     throw new IllegalArgumentException("Erro ao liberar o lock");
                 }
             }
         }
     }
+
+    // os métodos abrirCanal e fecharCanal são herdados da super classe
 
 }
